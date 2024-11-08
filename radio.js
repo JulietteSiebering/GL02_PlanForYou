b1c769276da9bc191609db80913fa740ccfd0a50
Klim200 <153085334+Klim200@users.noreply.github.com>
2024-11-08 17:52:52 +0100

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,11 +1,7 @@
 var Radio = {
-    play: function() {
-        console.log("Radio is now playing!");
-        return true;
-    },
-    stop: function() {
-        console.log("Radio has stopped!");
-        return true;
-    }
+check: function(){
+console.log("Radio Ready !")
+return true
 }
-module.exports = Radio;
+}
+module.exports = Radio
